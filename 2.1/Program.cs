5957f599cc41c005f15490abebf48b4038167db8
ALEX SHER <sheras85@mail.com>
2022-12-09 19:03:16 +0300

    stage

---
+++ 2.1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int n;
+Console.WriteLine(n = (a % 100 - a % 10)/10);
+
---
+++ 2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+if (a<=99)
+{
+   Console.WriteLine("Нет третьего числа"); 
+}
+else
+{
+do a = a / 10; 
+while (a > 999);
+n = a%10;
+Console.WriteLine(n);
+}
